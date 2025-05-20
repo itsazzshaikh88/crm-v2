439f6314d08c177e815cce41825a00d2f2e1da9a
Shaikh Azim <ajimsk00@gmail.com>
2025-05-20 11:25:30 +0530

    Email template: starter

--- application/controllers/Email.php
+++ application/controllers/Email.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Email extends CI_Controller
+class Email extends App_Controller
 {
     public function __construct()
     {
@@ -9,5 +9,29 @@ class Email extends CI_Controller
         $this->load->library('phpmailer_lib');
     }
 
-    
+    function send()
+    {
+        $emailViewConfig = [
+            'content_view' => 'account-created',
+            'heading' => "Your Zamil CRM Account is created.",
+        ];
+        $mailContent = $this->load->view('email-templates/layout', ['emailViewConfig' => $emailViewConfig], true);
+
+        $result = $this->send_email([
+            'to'        => 'shaikh.azim@olivesofts.com',
+            'subject'   => 'Test PHPMailer Email',
+            'message'   => $mailContent,
+            'from'      => 'workflowmailer@zamilplastic.com',   // Optional
+            'from_name' => 'WorkFlow Mailer',                   // Optional
+        ]);
+
+        if ($result['success']) {
+            echo "Email sent successfully to: " . $result['details']['to'];
+        } else {
+            echo "Email failed: " . $result['error'];
+        }
+        echo "</br>";
+
+        beautify_array($result);
+    }
 }
